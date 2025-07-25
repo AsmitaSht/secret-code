c7746de09ead87c6b14d2d5374f96e124d2935ac
AsmitaSht <shtasmita01@gmail.com>
2025-07-25 15:07:03 +0545

    Initialize

---
+++ decryption.py
@@ -0,0 +1,17 @@
+def decry(t):
+    if(len(t)<3):
+        return ''.join(reversed(t))
+    else:
+        t=t[3:-3]
+        a=t[-1]
+        t=t.replace(a,"",-1)
+        t=a+t
+        return t
+
+txt=input("enter the encrypted msg:")
+m=[]
+for i in txt.split():
+    r=decry(i)
+    m.append(r)
+msg=' '.join(m)
+print("The encrpted msg is ",msg)
---
+++ encryption.py
@@ -0,0 +1,26 @@
+import random
+import string
+
+def encry(txt):
+    if(len(txt)<3):
+        txt=''.join(reversed(txt))
+        return txt
+    else:
+        a=txt[0]
+        t=txt.replace(a,"",1)
+        t=t+a
+        for i in range(3):
+            r_char=random.choice(string.ascii_letters)
+            p_char=random.choice(string.ascii_letters)
+            t=r_char+t+p_char
+        txt=t
+        return txt
+
+txt=input("Enter the message:")
+m=[]
+for i in txt.split():
+    r=encry(i)
+    m.append(r)
+msg=' '.join(m)
+print("The encrpted msg is ",msg)
+
